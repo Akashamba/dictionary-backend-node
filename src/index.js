d2642665b6e4a7f3ed1fed24fa49dab4025b0c0c
Akash Ambashankar <53367477+Akashamba@users.noreply.github.com>
2020-12-13 22:26:16 +0530

    completed backend

--- src/index.js
+++ src/index.js
@@ -1,43 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const http = require("https");
-const cors  =require("cors");
+const cors = require("cors");
 
 const app = express();
-// app.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://m1gnj.csb.app/' );
-//   next();
-// })
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true })); // body-parser
 app.use(bodyParser.json()); // body-parser
 
-app.listen(process.env.PORT || 5000);
+app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.send(
-    'API hit successfully. Send request with word in params like "/api/run".'
-  );
+  res.send('API hit successfully. Send request with word in params like "/api/run".');
 });
 
 app.get("/api/:word", (req, res) => {
-  // , req.headers.app_id, req.headers.app_key
   extract(req.params.word, res);
 });
 
-function extract(word, this_response) {
-  // + '?fields=' + fields + '&strictMatch=' + strictMatch,
+// const app_id = "3a12c817";
+// const app_key = "5ad5f3e79690e46fd4adbd080cfe9883";
 
-  const app_id = "3a12c817";
-  const app_key = "5ad5f3e79690e46fd4adbd080cfe9883";
+const app_id = "1527fb95";
+const app_key = "21091ba0a0924ebb05f4207c4b57ee23";
+
+
+function extract(word, this_response) {
   const options = {
     host: "od-api.oxforddictionaries.com",
     port: "443",
     path: "/api/v2/entries/en-us/" + word,
     method: "GET",
     headers: {
-      app_id: app_id,
-      app_key: app_key
+      'app_id': app_id,
+      'app_key': app_key
     }
   };
 
@@ -48,44 +44,57 @@ function extract(word, this_response) {
       res.on("data", (d) => {
         body += d;
       });
-  
-      res.on("end", () => {
-        body = JSON.parse(body);
-        parsed.id = body.id;
-        parsed.word = body.word;
-        parsed.results = [];
 
+      res.on("end", () => {
+        body = JSON.parse(body)
         try {
 
-          for (var i = 0; i < body.results.length; i++) {
-            let obj = {};
-    
-            obj.definitions = [];
-            obj.synonyms = [];
-            obj.examples = [];
-    
-            obj.id = body.results[i].id;
-            for (var j = 0; j < body.results[i].lexicalEntries.length; j++) {
-              obj.type = body.results[i].lexicalEntries[j].lexicalCategory.text;
-              for (var k = 0; k < body.results[i].lexicalEntries[j].entries.length; k++) {
-                for (var l = 0; l < body.results[i].lexicalEntries[j].entries[k].senses.length; l++) {
-                  obj.definitions.push(body.results[i].lexicalEntries[j].entries[k].senses[l].definitions);
-                  obj.examples.push(body.results[i].lexicalEntries[j].entries[k].senses[l].examples);
-                  obj.synonyms.push(body.results[i].lexicalEntries[j].entries[k].senses[l].synonyms);
+          if(body.error)
+            throw new Error('no results found')
+
+          parsed.id = body.id;
+          parsed.word = body.word;
+          parsed.senses = [];
+          parsed.synonyms = [];
+
+          const {results} = body;
+
+          if(results)
+            for(let i=0; i<results.length; i++) {
+              if(results[i].lexicalEntries)
+                for(let j=0; j<results[i].lexicalEntries.length; j++) {
+                  if(results[i].lexicalEntries[j].entries)
+                    for(let k=0; k<results[i].lexicalEntries[j].entries.length; k++) {
+                      if(results[i].lexicalEntries[j].entries[k].pronunciations)
+                        for(let p=0; p<results[i].lexicalEntries[j].entries[k].pronunciations.length; p++){
+                          if(!parsed.pronunciation)
+                            try{parsed.pronunciation = results[i].lexicalEntries[j].entries[k].pronunciations[p].audioFile}catch(e){console.log('Pronunciation: ',e.message)}
+                        }
+                      if(results[i].lexicalEntries[j].entries[k].senses)
+                        for(let l=0; l<results[i].lexicalEntries[j].entries[k].senses.length; l++) {
+                          let sense = {};
+                          sense.examples = [];
+                          try{sense.definition = results[i].lexicalEntries[j].entries[k].senses[l].definitions[0]}catch(e){console.log('Definition: ', e.message)}
+                          if(results[i].lexicalEntries[j].entries[k].senses[l].examples)
+                            for(let m=0; m<results[i].lexicalEntries[j].entries[k].senses[l].examples.length; m++){
+                              try{sense.examples.push(results[i].lexicalEntries[j].entries[k].senses[l].examples[m].text)}catch(e){console.log('Examples: ', e.message)}
+                            }
+                          parsed.senses.push(sense);
+                          if(results[i].lexicalEntries[j].entries[k].senses[l].synonyms)
+                            for(let n=0; n<results[i].lexicalEntries[j].entries[k].senses[l].synonyms.length; n++){
+                              try{parsed.synonyms.push(results[i].lexicalEntries[j].entries[k].senses[l].synonyms[n].text)}catch(e){'Synonyms: ', console.log(e.message)}
+                            }
+                        }
+                    }
                 }
-              }
             }
-            parsed.results.push(obj);
-            console.log('done')
         }
-      }
-      catch (e) {
-        console.log(e.message)
-        parsed = body;
-      }
+        catch (e) {
+          console.log('Overall: ', e.message);
+          parsed.error = body.error;
+        }
+
         this_response.json(parsed);
-        // this_response.json(JSON.stringify(parsed, null, 0));
       });
     });
   }
-
